a015a16890680f8624b131ec05ee71a86650b8f8
kholis313354 <150056059+kholis313354@users.noreply.github.com>
2023-11-19 20:45:09 +0700

    Update word.js
    ubah pertanyaan

--- assets/js/word.js
+++ assets/js/word.js
@@ -1,50 +1,50 @@
 const wordList = [
   {
-    word: "jokowi",
-    hint: "Siapa Presiden Indonesia Sekarang?"
+    word: "algoritma",
+    hint: "serangkaian langkah atau aturan yang didefinisikan secara jelas dan sistematis untuk menyelesaikan masalah atau menjalankan suatu tugas disebut?"
   },
   {
-    word: "jakarta",
-    hint: "Dimana Ibukota Indonesia?"
+    word: "integer",
+    hint: "tipe data yang dipakai untuk menampung angka bulat positif maupun negatif seperti : 1, 45, dan -1945. di sebut dengan ?"
   },
   {
-    word: "agustus",
-    hint: "Bulan kemerdekaan Indonesia?"
+    word: "konstanta",
+    hint: "nilai numerik/angka atau karakter yang tetap di sebut dengan?"
   },
   {
-    word: "soekarno",
-    hint: "Siapa proklamator?"
+    word: "sorting",
+    hint: " proses mengatur elemen-elemen dalam suatu urutan tertentu, seperti menaik atau menurun. Algoritma sorting digunakan untuk mengurutkan data agar mudah dicari, diakses, atau diproses di sebut?"
   },
   {
-    word: "hatta",
-    hint: "Siapa wakil proklamator?"
+    word: "variabel",
+    hint: "Sebuah simbolik yang mewakili nilai atau lokasi memori dalam program?"
   },
   {
-    word: "belanda",
-    hint: "Perang kemerdekaan melawan?"
+    word: "colspan",
+    hint: "digunakan untuk menggabungkan sel-sel (kolom) horizontal di dalam baris tabel?"
   },
   {
-    word: "soekarno",
-    hint: "Siapa penulis teks proklamasi?"
+    word: "rowspan",
+    hint: "digunakan untuk menggabungkan sel-sel (baris) vertikal di dalam kolom tabel.?"
   },
   {
-    word: "merdeka",
-    hint: "Nama istana pertama?"
+    word: "komentar",
+    hint: "bagaimana cara Gunakan sintaks  untuk menambahkan komentar di dalam kode HTML. Contoh:?"
   },
   {
-    word: "soekarno",
-    hint: "Revolusi mental dirintis?"
+    word: "br",
+    hint: "digunakan untuk membuat pemisah baris (line break) di dalam teks atau konten HTML,Contoh:?"
   },
   {
-    word: "jakarta",
-    hint: "Proklamasi dibacakan di?"
+    word: "meta charset UTF-8",
+    hint: "Tag  digunakan untuk mendefinisikan karakter set yang digunakan oleh halaman web.?"
   },
   {
-    word: "kartini",
-    hint: "Tokoh perjuangan wanita?"
+    word: "ul",
+    hint: "Tag <ul> digunakan untuk membuat daftar tidak terurut (unordered list), di mana setiap item diberi simbol atau tanda bulat.?"
   },
   {
-    word: "diponegoro",
-    hint: "Pangeran yang melawan kolonial?"
+    word: "ol",
+    hint: "Tag  digunakan untuk membuat daftar terurut (ordered list), di mana setiap item diberi nomor atau simbol.?"
   },
 ];
